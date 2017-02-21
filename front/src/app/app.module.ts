1fdd94fff2833b9a7c5b2c747bdefdc52fe4caf1
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-02-21 19:43:38 +0200

    front - page comp

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,12 +1,44 @@
-import { NgModule }        from '@angular/core';
-import { BrowserModule }   from '@angular/platform-browser';
-import { AppComponent }    from './app.component';
-import { MaterialModule }  from '@angular/material';
-import 'hammerjs';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+import {MaterialModule} from "@angular/material";
+import "hammerjs";
+import {Routes, RouterModule} from "@angular/router";
+import {PageComponent} from "./page/page.component";
+import {PageNotFoundComponent} from "./error/error.component";
+
+const routes: Routes = [
+  {
+    path: 'pages',
+    component: PageComponent,
+    data: {
+      title: 'Pages'
+    }
+  },
+  {
+    path: '',
+    redirectTo: '/pages',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, MaterialModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    MaterialModule
+  ],
+  declarations: [
+    AppComponent,
+    PageComponent,
+    PageNotFoundComponent
+  ],
+  bootstrap:[
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ front/src/app/page/page.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PageService} from "./page.service";
+
+@Component({
+    selector: 'page',
+    templateUrl: './page.component.html',
+    styleUrls: ['./page.component.css']
+})
+export class PageComponent implements OnInit {
+
+    private title;
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private router: Router,
+        private service: PageService
+    ) {}
+
+    ngOnInit(): void {
+        this.title = this.activatedRoute.data.subscribe((data: { title: 'Pages'}) => {
+            this.title = data.title;
+        });
+    }
+
+}
