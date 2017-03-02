da855a083541d26ca7ac4acb97ceb103a2c53a7f
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-03-02 23:59:27 +0200

    has children

--- src/main/java/com/tobilko/page/entity/PageChildren.java
+++ src/main/java/com/tobilko/page/entity/PageChildren.java
@@ -1,20 +1,27 @@
 package com.tobilko.page.entity;
 
 import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 /**
  *
  * Created by Andrew Tobilko on 02.03.17.
+ *
  */
 @Embeddable
 public class PageChildren {
 
+    @OneToMany(mappedBy = "Page")
     private List<Page> children;
 
     /**
      * Defines how children should be obtained from the database.
      */
     private SortCriterion criterion;
+
+    public boolean hasChildren() {
+        return !(children == null || children.isEmpty());
+    }
 
 }
